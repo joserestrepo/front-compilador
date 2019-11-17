9c90a5774cf8141ab0be34f39e82a25c2f37b3bb
joserestrepo <joserestrepo301@gmail.com>
2019-11-16 19:48:30 -0500

    subiendo proyecto a git

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AceEditorModule } from 'ng2-ace-editor';
+
+
+
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { LoginComponent } from './components/login/login.component';
+import { EditorComponent } from './components/editor/editor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    LoginComponent,
+    EditorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AceEditorModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
